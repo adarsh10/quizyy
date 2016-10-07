a6820705e6a6d0ace403fe47e96b616c691cc242
adarsh <rajshahbvm@yahoo.com>
2016-10-07 23:38:10 +0530

    removed unused code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,13 +6,7 @@ import play.mvc.Result;
 
 public class Application extends Controller {
 
-    private static boolean isDevMode = Play.isDev(Play.current());
-
     public static Result index() {
-        String sessionId = session().get("sessionId");
-        if (sessionId != null) {
-            return redirect("/dashboard");
-        }
         return ok(views.html.index.render());
     }
 }
